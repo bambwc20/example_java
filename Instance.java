630528fda686dfa2b284439a307f78eca17a0050
DESKTOP-7SVMBFP\zhxjf <bambwc20@naver.com>
2021-08-06 23:19:49 +0900

    질문 모달

--- Instance.java
+++ Instance.java
@@ -2,11 +2,6 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-import java.io.PrintWriter;
-import java.io.IOException;
-import java.io.FileNotFoundException;
-import java.lang.Exception;
-
 public class Instance {
     public static void main(String[] args) throws IOException, FileNotFoundException {
         PrintWriter p1 = new PrintWriter("result1.txt");
